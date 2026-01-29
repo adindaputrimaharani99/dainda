b370a3b900ead9221a77ba21f63bfe0add48e040
adindaputrimaharani99 <25120664438@mhs.unesa.ac.id>
2026-01-29 11:31:47 +0700

    update

--- dist/js/script.js
+++ dist/js/script.js
@@ -20,13 +20,13 @@ window.onscroll = function(){
 const hamburger = document.querySelector('#hamburger');
 const navMenu = document.querySelector('#nav-menu');
 
-hamburger.addEventListener('click', function(){
+hamburger.addEventListener('click', function(e){
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 });
 
 //klik di luar hamburger
-window.addEventListener('click', function(){
+window.addEventListener('click', function(e){
     if(e.target != hamburger && e.target != navMenu){
         hamburger.classList.remove('hamburger-active');
         navMenu.classList.add('hidden');
